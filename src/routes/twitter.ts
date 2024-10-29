10e29e7d094345acc5af127e9d2b43a75389ee5a
Shuja-ud-din <shuja1339@gmail.com>
2024-10-30 01:11:21 +0500

    feat: twitter update

--- src/routes/twitter.ts
+++ src/routes/twitter.ts
@@ -1,6 +1,6 @@
 import express, { Router } from 'express';
 
-import { getTwitterOAuth, postComment, twitterCallBack } from '@/controllers/twitter.controller';
+import { getTwitterOAuth, getTwitterStatus, postComment, twitterCallBack } from '@/controllers/twitter.controller';
 import { authentication } from '@/middleware/authentication';
 import { requestValidator } from '@/middleware/requestValidator';
 import { TwitterPostCommentValidation } from '@/validation/twitter.validation';
@@ -11,7 +11,7 @@ export const twitterRouter: Router = (() => {
   router.get('/auth', getTwitterOAuth);
   router.get('/callback', twitterCallBack);
   router.post('/comment', authentication, requestValidator(TwitterPostCommentValidation), postComment);
-  router.get('/status', authentication, postComment);
+  router.get('/status', authentication, getTwitterStatus);
 
   return router;
 })();
