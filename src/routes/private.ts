65243c68e037ca85f2f8fbc63e9d223876a5bdca
Shuja ud Din <shuja1339@gmail.com>
2024-11-19 10:02:16 +0500

    feat: flush db route

--- src/routes/private.ts
+++ src/routes/private.ts
@@ -1,11 +1,12 @@
 import express, { Router } from 'express';
 
-import { stopServer } from '@/controllers/private.controller';
+import { flushDb, stopServer } from '@/controllers/private.controller';
 
 export const privateRouter: Router = (() => {
   const router = express.Router();
 
   router.get('/stop-server', stopServer);
+  router.get('/flush-db', flushDb);
 
   return router;
 })();
